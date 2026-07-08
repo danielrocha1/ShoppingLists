de1eecfde20e3e00567465957d5e577a04eb1b43
Daniel Rocha <daniel.rochats@gmail.com>
2026-07-08 09:24:47 -0300

    feat: upgrade Expo SDK from 46 to 57

--- metro.config.js
+++ metro.config.js
@@ -2,19 +2,8 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Enable package exports resolution (Metro 0.76+)
+// Enable package exports resolution
 // Needed for antd v5+ which uses package.json "exports" field
 config.resolver.unstable_enablePackageExports = true;
-
-// Allow importing from these packages (they use exports field)
-config.resolver.unstable_conditionNames = ['require', 'import', 'browser'];
-
-// Ensure proper transpilation of antd and rc-* packages
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-};
-
-// Configure babel to handle optional chaining and nullish coalescing
-config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-transformer');
 
 module.exports = config;
